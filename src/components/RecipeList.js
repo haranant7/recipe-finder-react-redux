32ab838a7da529cb31c8398c741b1ccfbb8d3043
A <haria@informatica.com>
2019-01-31 09:51:42 +0530

    Initial Commit

---
+++ src/components/RecipeList.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import RecipeItem from './RecipeItem';
+import { Link } from 'react-router-dom';
+
+class RecipeList extends Component{
+    render(){
+        return (
+            <div>
+                {
+                    this.props.favoriteRecipe.length > 0 ?
+                        <h4 className="link"><Link to='/favorites'>Favorites</Link></h4>
+                    :
+                        <div />
+                }
+                
+                {   
+                    this.props.recipes.map((recipe,index) => {
+                        return(
+                            <RecipeItem 
+                                key = {index} 
+                                recipe = {recipe} 
+                                favoriteButton={true}
+                            />
+                        )
+                    })
+                }
+            </div>
+        )
+    }
+}
+
+function mapStateToProps(state){
+    return state;
+}
+
+export default connect(mapStateToProps, null)(RecipeList);
---
+++ src/reducers/index.js
@@ -0,0 +1,25 @@
+import { combineReducers } from 'redux';
+import { SET_RECIPES, FAVORITE_RECIPE } from '../actions';
+
+function recipes(state = [], action){
+    switch(action.type){
+        case SET_RECIPES:
+            return action.items;
+        default:
+            return state;
+    }
+}
+
+function favoriteRecipe(state = [], action){
+    switch(action.type){
+        case FAVORITE_RECIPE:
+            state = [...state,action.recipe];
+            return state;
+        default:
+            return state;
+    }
+}
+
+const rootReducer = combineReducers({ recipes, favoriteRecipe });
+
+export default rootReducer;
